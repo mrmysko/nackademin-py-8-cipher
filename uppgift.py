bfebd1ea4024ceb3c02b8f9b68a865aa664f9fcf
Robin Kavenius <robin.kavenius@yh.nackademin.se>
2024-03-13 13:28:25 +0100

    Finish substitution cipher

--- uppgift.py
+++ uppgift.py
@@ -1,16 +1,24 @@
 # Skriv endast definitioner här på denna indenteringsnivå! Det är viktigt att du
 # namnger funktioner, klasser och variabler exakt med de namn som står i
 # beskrivningen.
-import collections
+import collections.abc
 
 
-class InvalidSubstitutionCipher(Exception):
+class InvalidSubstitutionCipher(ValueError):
     pass
 
 
 class SubstitutionCipher:
-    def __init__(self, map: [(str, str)]):
+
+    def __init__(self, map: [(str, str)]):  # type: ignore
+        # Save input to object-specific variables.
         self.map = map
+        # Keep track of tuple values.
+        self.cipher_chars = []
+
+        # If map is not a sequence raise error.
+        if not isinstance(self.map, collections.abc.Sequence):
+            raise TypeError(f"{self.map} is not a sequence.")
 
         for i in self.map:
             # Check if tuple is not two values.
@@ -18,18 +26,27 @@ class SubstitutionCipher:
                 raise ValueError(f"{i} invalid tuple length")
 
             # Check if tuple value is not a string.
-            if not isinstance(i[0], str) or not isinstance(i[1], str):
-                raise ValueError("Not a string.")
+            for x in i:
+                if x in self.cipher_chars:
+                    raise InvalidSubstitutionCipher(f"{x} in map multiple times.")
+                else:
+                    self.cipher_chars.append(x)
 
-        """if not isinstance(map, collections.abc.Sequence):
-            raise TypeError("Null-string not allwed.")
-        elif some shit appears multiple times.
-            raise InvalidSubstitutionCipher("Invalid cipher.")"""
+                if len(x) != 1:
+                    raise ValueError(f"{x} is not 1 character long.")
+                if not isinstance(x, str):
+                    raise TypeError(f"{x} is not a string.")
 
     def substitute(self, text: str) -> str:
-        # Works one way...but how do I do it the other way around?
-        for enc_k, dec_k in self.map:
-            text = text.replace(enc_k, dec_k)
+        """Substitute chars in text with letters from self.map tuple-pairs."""
+        if not isinstance(text, str):
+            raise TypeError("text is not a string.")
+
+        for v1, v2 in self.map:
+            if v1 in text:
+                text = text.replace(v1, v2)
+            elif v2 in text:
+                text = text.replace(v2, v1)
 
         return text
 
@@ -50,6 +67,12 @@ if __name__ == "__main__":
     # Exempel:
     # minklass = Klass()
     # print(klass.leet("hejsan")
-    test = SubstitutionCipher([("a", "m"), ("b", "n"), (1, "g")])
-    print(test.substitute("abba"))  # mnnm
-    print(test.substitute("mnnm"))  # abba
+    test = SubstitutionCipher([("a", "m"), ("b", "n")])
+    print(f'{test.substitute("abba")}')  # mnnm
+    print(f'{test.substitute("mnnm")}')  # abba
+
+    test2 = SubstitutionCipher(
+        [("h", "f"), ("a", "b"), ("l", "g"), ("d", "c"), ("n", "o"), ("t", "p")]
+    )
+    print(f'{test2.substitute("Halloj")}')
+    print(f'{test2.substitute("Hbggnj")}')
